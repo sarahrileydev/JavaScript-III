34f130d821a144a36eb864e527eccca57a5b688c
sriley <sarahriley.dev@gmail.com>
2019-02-27 15:08:17 -0500

    definitions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When used globally, "this" points towards the window as the Object.
+* 2. When used implicitally, "this" points towards the object preceding the dot in a function invocation.
+* 3. When used to create new Objects via a constructor function, "this" refers to the specific use that gets passed through the constructor function.
+* 4. When used explicitly, "this" is defined inside the call() or apply() method.
 *
 * write out a code example of each explanation above
 */
